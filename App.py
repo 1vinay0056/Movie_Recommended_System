179480ac8706d64c32c5c0f7e6797c2abf07a04d
1vinay0056 <vinaykumarmth78@gmail.com>
2026-06-11 16:17:24 +0530

    Improved UI with Netflix-style navbar and movie layout

--- App.py
+++ App.py
@@ -287,7 +287,7 @@ st.markdown("""
 🎬 FilmFlow
 </h1>
 
-<p style='color:#9ca3af;font-size:18px;margin-top:0px;margin-bottom:5px;'>
+<p style='color:#9ca3af;font-size:18px;margin-top:0px;margin-bottom:3px;'>
 Discover Trending Movies & Personalized Recommendations
 </p>
 """, unsafe_allow_html=True)
